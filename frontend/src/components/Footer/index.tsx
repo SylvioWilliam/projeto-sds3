e118f62b6f3e214c80a96f9e8f6802b7b69a80e2
SylvioWilliam <williamsylvio@gmail.com>
2021-09-09 21:29:40 -0300

    Basic Static Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/sylvio-william-val%C3%A9rio-de-moraes-8a3a85a1/" target="_blank" rel="noreferrer">Sylvio William</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
